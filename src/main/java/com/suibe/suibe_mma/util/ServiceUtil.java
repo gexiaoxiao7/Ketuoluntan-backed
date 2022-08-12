3d2a6f6011e8ee503919df5382908b2de19c2f9f
crt1314 <3378310437@qq.com>
2022-08-12 16:01:34 +0800

    fix

--- src/main/java/com/suibe/suibe_mma/util/ServiceUtil.java
+++ src/main/java/com/suibe/suibe_mma/util/ServiceUtil.java
@@ -142,11 +142,11 @@ public class ServiceUtil {
      */
     public static Reply checkReplyId(Long id, ReplyService replyService) throws ReplyException {
         if (id == null) {
-            ReplyExceptionEnumeration.REPLY_ID_IS_NULL.throwTopicException();
+            ReplyExceptionEnumeration.REPLY_ID_IS_NULL.throwReplyException();
         }
         Reply reply = replyService.getById(id);
         if (reply == null) {
-            ReplyExceptionEnumeration.REPLY_ID_IS_WRONG.throwTopicException();
+            ReplyExceptionEnumeration.REPLY_ID_IS_WRONG.throwReplyException();
         }
         return reply;
     }
